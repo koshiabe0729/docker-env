824da8f0568d1f0d4b21595763a6c51026ca34d9
koshiabe0729 <koushiabe5@gmail.com>
2025-11-13 00:19:37 +0900

    環境構築完了

--- app/routes/web.php
+++ app/routes/web.php
@@ -1,6 +1,19 @@
 <?php
 
+<<<<<<< Updated upstream
 use Illuminate\Support\Facades\Route;
+=======
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+>>>>>>> Stashed changes
 
 Route::get('/', function () {
     return view('welcome');
